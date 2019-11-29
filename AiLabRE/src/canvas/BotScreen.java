1db211ba48095dea588b6d6409d7ce9f2aff9ef1
Fabian <unconfigured@unregistered.org>
2019-11-29 21:30:05 +0100

    Artistic Vision

--- AiLabRE/src/canvas/BotScreen.java
+++ AiLabRE/src/canvas/BotScreen.java
@@ -29,7 +29,7 @@ public class BotScreen extends Canvas {
 		Graphics2D g2d = (Graphics2D) bi.getGraphics();
 		g2d.setColor(Color.red);
 		g2d.drawOval((int)bot.getPos().getX() - RAD, (int)bot.getPos().getY() - RAD, RAD*2, RAD*2);
-		g2d.drawLine((int)bot.getPos().getX() + (int)bot.getDir().getNormalized().getX() * RAD, (int)bot.getPos().getY() + (int)bot.getDir().getNormalized().getY() * RAD, (int)bot.getPos().getX() + (int)bot.getDir().getNormalized().getX() * -RAD, (int)bot.getPos().getY() + (int)bot.getDir().getNormalized().getY() * -RAD);
+		drawLine(g2d,Vector2.add( bot.getPos(), bot.getDir().getNormalized().mult(RAD)),Vector2.add( bot.getPos(), bot.getDir().getNormalized().mult(-RAD)));
 		for (TrusterInterface t : bot.getAllTrusters()) {
 			Vector2 pos = t.getAbsolutePos();
 			Vector2 ruler = Vector2.turnDeg(new Vector2(0, SQR), Vector2.getAngle(new Vector2(0, 1),t.getDirection()));
@@ -37,7 +37,7 @@ public class BotScreen extends Canvas {
 			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, -30)),Vector2.add(pos ,Vector2.turnDeg(ruler, -150)));
 			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, -150)),Vector2.add(pos ,Vector2.turnDeg(ruler, 150)));
 			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, 30)),Vector2.add(pos ,Vector2.turnDeg(ruler, 150)));
-			Vector2 top = pos;
+			Vector2 top = Vector2.add(pos, Vector2.add(Vector2.turnDeg(ruler, -150), Vector2.turnDeg(ruler, 150)).mult(0.5));
 			drawLine(g2d,Vector2.add(Vector2.add(bot.getPos().mult(-1),top).getNormalized().mult(RAD), bot.getPos() ), top);
 		}
 		g.drawImage(bi, 0, 0, null);
