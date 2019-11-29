0a0550baf1c2295ffd29ef06b06f570af31a4cb7
Fabian <unconfigured@unregistered.org>
2019-11-29 21:06:23 +0100

    Turbine rot

--- AiLabRE/src/canvas/BotScreen.java
+++ AiLabRE/src/canvas/BotScreen.java
@@ -13,7 +13,7 @@ import utils.Vector2;
 public class BotScreen extends Canvas {
 	
 	private static final int RAD = 30;
-	private static final int SQR = 30;
+	private static final int SQR = 10;
 	
 	private Bot bot;
 	
@@ -28,16 +28,17 @@ public class BotScreen extends Canvas {
 		BufferedImage bi = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
 		Graphics2D g2d = (Graphics2D) bi.getGraphics();
 		g2d.setColor(Color.red);
-		g2d.drawOval((int)bot.getPos().getX()- RAD, (int)bot.getPos().getY() - RAD, RAD*2, RAD*2);
+		g2d.drawOval((int)bot.getPos().getX() - RAD, (int)bot.getPos().getY() - RAD, RAD*2, RAD*2);
 		g2d.drawLine((int)bot.getPos().getX() + (int)bot.getDir().getNormalized().getX() * RAD, (int)bot.getPos().getY() + (int)bot.getDir().getNormalized().getY() * RAD, (int)bot.getPos().getX() + (int)bot.getDir().getNormalized().getX() * -RAD, (int)bot.getPos().getY() + (int)bot.getDir().getNormalized().getY() * -RAD);
 		for (TrusterInterface t : bot.getAllTrusters()) {
 			Vector2 pos = t.getAbsolutePos();
-			Vector2 ruler = Vector2.turnDeg(new Vector2(SQR, 0), Vector2.getAngle(new Vector2(1, 0),t.getDirection()));
-			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, -30)), Vector2.add(pos ,Vector2.turnDeg(ruler, 30)));
+			Vector2 ruler = Vector2.turnDeg(new Vector2(0, SQR), Vector2.getAngle(new Vector2(0, 1),t.getDirection()));
+			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, -30)), Vector2.add(pos ,Vector2.turnDeg(ruler, -30)));
 			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, -30)),Vector2.add(pos ,Vector2.turnDeg(ruler, -150)));
-			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, -150)),Vector2.add(pos ,Vector2.turnDeg(ruler, -150)));
+			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, -150)),Vector2.add(pos ,Vector2.turnDeg(ruler, 150)));
 			drawLine(g2d,Vector2.add(pos ,Vector2.turnDeg(ruler, 30)),Vector2.add(pos ,Vector2.turnDeg(ruler, 150)));
-			drawLine(g2d, pos, bot.getPos());
+			Vector2 top = pos;
+			drawLine(g2d,Vector2.add(Vector2.add(bot.getPos().mult(-1),top).getNormalized().mult(RAD), bot.getPos() ), top);
 		}
 		g.drawImage(bi, 0, 0, null);
 	}
